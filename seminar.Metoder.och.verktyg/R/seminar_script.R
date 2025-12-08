ded58e645c37314a7e8b201792a68b5c3416df8e
Alexandra <d22alehe@student.his.se>
2025-12-08 16:56:31 +0100

    change so now both the sample table and filtered counts are returned. Also printed out that the filtering worked, and before and after count instead of returning the dimensions

--- seminar.Metoder.och.verktyg/R/seminar_script.R
+++ seminar.Metoder.och.verktyg/R/seminar_script.R
@@ -14,19 +14,27 @@ gene_table <- function(count_file, sample_file ){
   sample_table$individual <- factor(sample_table$individual)
   sample_table$sex <- factor(sample_table$sex)
   sample_table$diseas <- factor(sample_table$diseas)
-
   a <- colnames(count_table) == rownames(sample_table)
-
-  return(count_table)}
+  gene_data <- list(count_table,sample_table))
+  return(gene_data)}
 
 
 
 low_gene_filtering <- function(cutoff) {
-  countdata <- gene_table("E-MTAB-2523.counts.txt", "E-MTAB-2523_sample table.txt")
-  filt <- rowMeans(log2(edgeR::cpm(countdata)+1))
+  gene_data <- gene_table("E-MTAB-2523.counts.txt", "E-MTAB-2523_sample table.txt")
+  count_table <- gene_data[[1]]
+  sample_table <- gene_data[[2]]
+  filt <- rowMeans(log2(edgeR::cpm(count_table)+1))
   sum(filt <= 1)
-  count_table_filt <- countdata[filt > cutoff,]
-  return(list(dim(countdata), dim(count_table_filt)))}
+  count_table_filt <- count_table[filt > cutoff,]
+
+  msg <- paste("filtering works! data befoe filtering ",
+               paste(dim(count_table), collapse = "x"),
+               "after filtering",
+                paste(dim(count_table_filt), collapse = "x"))
+
+  message(msg)
+  return(list(count_table_filt, sample_table))}
 
 low_gene_filtering(1)
-#comentar
+
