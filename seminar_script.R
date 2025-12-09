1e1b98fbfadaf17f62da97d204c4cb1a65427d4c
NalaLilly <98475868+NalaLilly@users.noreply.github.com>
2025-12-09 17:17:31 +0100

    test changes in seminar script

---
+++ seminar_script.R
@@ -0,0 +1,26 @@
+library(devtools)
+use_package("edgeR")
+
+gene_table <- function(count_file, sample_file ){
+  count_table <- read.table(count_file, header = TRUE, as.is = TRUE, row.names = 1, sep = "\t")
+  sample_table <- read.table(sample_file, header = TRUE, as.is = TRUE, row.names = 1, sep = "\t")
+
+  sample_table$individual <- factor(sample_table$individual)
+  sample_table$sex <- factor(sample_table$sex)
+  sample_table$diseas <- factor(sample_table$diseas)
+
+  a <- colnames(count_table) == rownames(sample_table)
+
+  return(list(head(count_table), sample_table, a))}
+
+gene_table("E-MTAB-2523.counts.txt", "E-MTAB-2523_sample table.txt")
+
+low_gene_filtering <- function(cutoff) {
+filt <- rowMeans(log2(cpm(count_table)+1))
+sum(filt <= 1)
+count_table_filt <- count_table[filt > cutoff,]
+dim(count_table)
+dim(count_table_filt)
+}
+
+low_gene_filtering(1)
