e0359bac97a944c3ce57c065adef42a98f01711e
Alexandra <d22alehe@student.his.se>
2025-12-10 16:16:27 +0100

    dowlode org.hs.eg.dp package. transform gene ID to Entrez ID and remove NAs and duplicates

--- seminar_script_enrichmentAnlasys.R
+++ seminar_script_enrichmentAnlasys.R
@@ -1,1 +1,24 @@
-#testar
+library(devtools)
+
+BiocManager::install("org.Hs.eg.db")
+use_package("org.Hs.eg.db")
+
+ORA <- function(){
+  a <- run_dge_edger(count_table, sample_table,
+                group_col     = "disease",
+                case_label    = "carcinoma",
+                control_label = "normal",
+                fdr_cutoff    = 0.05,
+                lfc_cutoff    = 1)
+
+  signficant_genes <- a[[2]]
+  genes_names <- rownames(signficant_genes)
+  genes_Entrez <- org.Hs.eg.db::mapIds(org.Hs.eg.db,
+                                            keys=genes_names,
+                                            keytype="SYMBOL",
+                                            column="ENTREZID")
+  genes_cl10_Entrez<-genes_cl10_Entrez[!duplicated(genes_cl10_Entrez) &
+                                         !duplicated(genes_cl10_Entrez,fromLast=TRUE)]
+  paste0("Number of genes in after removing elements with duplicated names and or NAs = ",
+         length(genes_cl10_Entrez))
+  }
