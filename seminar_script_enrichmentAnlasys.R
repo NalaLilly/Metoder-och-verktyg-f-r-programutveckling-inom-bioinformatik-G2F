2c1e87475dfe0a7ddc31e4d2978253145843abd4
Alexandra <d22alehe@student.his.se>
2025-12-12 12:53:51 +0100

    Adding a document file GO analysis is working KEGG is not fixed yet

--- seminar_script_enrichmentAnlasys.R
+++ seminar_script_enrichmentAnlasys.R
@@ -1,9 +1,42 @@
 library(devtools)
 
 BiocManager::install("org.Hs.eg.db")
-use_package("org.Hs.eg.db")
+BiocManager::install("AnnotationDbi")
+BiocManager::install("clusterProfiler")
+BiocManager::install("ggplot2")
 
-ORA <- function(){
+use_package("org.Hs.eg.db")
+use_package("AnnotationDbi")
+use_package("clusterProfiler")
+use_package("ggplot2")
+
+count_table <- read.table("E-MTAB-2523.counts.txt", header = TRUE, as.is = TRUE, row.names = 1, sep = "\t")
+sample_table <- read.table("E-MTAB-2523_sample table.txt", header = TRUE, as.is = TRUE, row.names = 1, sep = "\t")
+
+GO_pathway <- function(organism = org.Hs.eg.db, gene_type = "SYMBOL"){
+  statical_analysis<- run_dge_edger(count_table, sample_table,
+                     group_col     = "disease",
+                     case_label    = "carcinoma",
+                     control_label = "normal",
+                     fdr_cutoff    = 0.05,
+                     lfc_cutoff    = 1)
+
+  significant_genes <- statical_analysis[[2]]
+  genes_names <- rownames(significant_genes)
+  library(org.Hs.eg.db)
+
+  GO <- clusterProfiler::enrichGO(gene = genes_names,
+                            OrgDb = organism,
+                            keyType = gene_type,
+                            ont = "BP",
+                            pvalueCutoff = 0.05)
+  head(GO@result[,c(2,3,5)])}
+
+
+GO_pathway(organism = org.Hs.eg.db, gene_type = "SYMBOL")
+
+
+KEGG_pathway <- function(organism = org.Hs.eg.db){
   a <- run_dge_edger(count_table, sample_table,
                 group_col     = "disease",
                 case_label    = "carcinoma",
